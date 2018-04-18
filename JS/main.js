3a3b09792c9b48619decdffc7eac9c396521eac0
Natasha Ramburrun <natasharamburrun@natashas-mbp.home>
2018-04-18 15:52:44 +0100

    created a game logic

--- JS/main.js
+++ JS/main.js
@@ -1,22 +1,33 @@
 
 
 var cards = ['queen', 'queen', 'king', 'king'];
+
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne)
-console.log("user flipped" + cardOne);
 
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo)
-console.log("user flipped" + cardTwo);
-
-if (cardsInPlay.length === 2) { 
-
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again");
+var checkForMatch = function () {
+ if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+  }	
+};
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
 	}
-}
+};
+
+flipCard(0);
+
+flipCard(2);
+
+
+
+
+
+	
+
+
 
